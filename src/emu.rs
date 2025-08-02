1753d71e6349996e3400150ed73cf1e1de3893d1
chyizheng <chyizheng@tencent.com>
2025-08-03 07:28:31 +0800

    add unit test

--- src/emu.rs
+++ src/emu.rs
@@ -13,17 +13,14 @@ pub struct Emu<'a> {
 }
 
 impl<'a> Emu<'a> {
-    pub fn run(filename: &String) -> std::io::Result<()> {
-        // println!("reading cart {:?}", filename);
-        let mut cartridge = Cartridge::load_cartridge(filename)?;
-        // println!("loaded cart {:#?}", cartridge);
+    pub fn run(cartridge: &mut Cartridge) {
         let mut wram = Ram::<0x2000>::create();
         let mut hram = Ram::<0x80>::create();
         let serial = Rc::new(RefCell::new(Serial::create()));
-        let mut io = IO::create(Rc::clone(&serial));
+        let mut io = IO::create(serial);
 
         let bus = Bus {
-            cartridge: &mut cartridge,
+            cartridge,
             wram: &mut wram,
             hram: &mut hram,
             io: &mut io,
@@ -33,23 +30,65 @@ impl<'a> Emu<'a> {
 
         cpu.init();
 
-        let mut dbg_msg = String::new();
-
         while !cpu.halted {
             if !cpu.step() {
                 println!("CPU stopped");
             }
-            let mut serial = serial.borrow_mut();
-            if serial.control == 0x81 {
-                dbg_msg.push(serial.data as char);
-                serial.control = 0;
-                println!("DBG: {:?}", dbg_msg);
-                if dbg_msg.ends_with("Passed\n") {
-                    panic!();
-                }
-            }
         }
+    }
+}
 
-        Ok(())
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    impl<'a> Emu<'a> {
+        pub fn run_test(filename: String, limit: usize) -> std::io::Result<()> {
+            let mut cartridge = Cartridge::load_cartridge(&filename)?;
+            let mut wram = Ram::<0x2000>::create();
+            let mut hram = Ram::<0x80>::create();
+            let serial = Rc::new(RefCell::new(Serial::create()));
+            let mut io = IO::create(Rc::clone(&serial));
+
+            let bus = Bus {
+                cartridge: &mut cartridge,
+                wram: &mut wram,
+                hram: &mut hram,
+                io: &mut io,
+            };
+
+            let mut cpu = CpuContext::create(bus);
+
+            cpu.init();
+
+            let mut dbg_msg = String::new();
+            let mut cycles: usize = 0;
+
+            while cycles < limit && !cpu.halted {
+                if !cpu.step() {
+                    println!("CPU stopped");
+                }
+                let mut serial = serial.borrow_mut();
+                if serial.control == 0x81 {
+                    dbg_msg.push(serial.data as char);
+                    serial.control = 0;
+                    if dbg_msg.ends_with("Passed\n") {
+                        return Ok(());
+                    }
+                }
+                cycles += 1
+            }
+            panic!("failed")
+        }
+    }
+
+    #[test]
+    fn special() -> std::io::Result<()> {
+        Emu::run_test("./roms/01-special.gb".into(), 10000000)
+    }
+
+    #[test]
+    fn interrupts() -> std::io::Result<()> {
+        Emu::run_test("./roms/02-interrupts.gb".into(), 10000000)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,18 @@ mod io;
 mod ram;
 mod utils;
 
+use cartridge::Cartridge;
 use emu::Emu;
+use std::env;
 
 fn main() -> std::io::Result<()> {
-    // let mut buffer = "/Users/chyizheng/workspace/gbemu-rs/roms/Wario_Land_3.gbc".into();
-    // let buffer = "/Users/chyizheng/workspace/gbemu-rs/roms/tetris.gb".into();
-    let buffer = "/Users/chyizheng/workspace/gbemu-web/roms/05-op rp.gb".into();
-    Emu::run(&buffer)?;
-    Ok(())
+    if let Some(filename) = env::args().nth(1) {
+        println!("reading cart {:?}", filename);
+        let mut cartridge = Cartridge::load_cartridge(&filename)?;
+        println!("loaded cart {:#?}", cartridge);
+        Emu::run(&mut cartridge);
+        Ok(())
+    } else {
+        panic!("must pass filename")
+    }
 }
